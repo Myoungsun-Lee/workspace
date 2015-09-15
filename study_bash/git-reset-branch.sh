cd5395706661121e16f88ca67afd70c8c72f8ed1
Myoungsun Lee <ohmygod0327@gmail.com>
2015-09-15 10:53:34 +0900

    Add study_bash directory.
    It contains git-update and git-reset-branch
    Please refer to HtoU(How to Use).

---
+++ study_bash/git-reset-branch.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+SRC_DIR=$(pwd)
+LOG_DIR=$SRC_DIR/log
+BRANCH=$1
+CHECK=false
+
+if [ -z $BRANCH ]; then
+    echo -e "Unkown branch ==> NULL";
+    exit 0;
+fi
+
+
+for file in */ ;
+do
+	echo -e "We are in $file";
+	cd $SRC_DIR/$file
+    
+    if [ -d $SRC_DIR/$file/.git ]; then
+        list=`git show-ref | grep .*origin.*\.$BRANCH | sed "s/.*origin\/\(.*\)/\1/g"` 
+        if [ -z "$list" ]; then 
+            echo -e "There is no branch about $BRANCH \n";
+            continue;
+        fi
+        let i=0
+        let j=0
+
+        echo -e "===branch list==="
+        for num in $list; do
+            let i++
+            echo -e "$num"
+        done
+        echo "================="
+        
+        for branch in $list ;
+        do
+          let j++
+          echo -e "Do you want to checkout $branch?[y/n]" ; read anw
+          if [ $anw = 'y' ] || [ $j -eq $i ] ; then break;
+          elif [ $anw = 'n' ]; then continue;
+          else echo "Wrong anwser!! Go to next branch!!"; continue;
+          fi
+        done
+
+        if [ $anw != 'y' ] && [ $j -eq $i ]; then 
+            echo -e "Go to next directory\n"
+            continue;
+        fi
+        
+        echo -e "...Changing branch to $branch";
+
+		git checkout $branch
+		echo -e "$file completed to change branch\n"
+        echo -e "go to next directory\n"
+	else
+        echo -e "This is not git directory. Go to next..\n"   
+		continue;
+	fi
+
+done
+
---
+++ study_bash/git-update.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+SRC_DIR=$(pwd)
+LOG_DIR=$SRC_DIR/log
+
+<<COMMENT
+cd $LOG_DIR
+if [ `pwd` != "$LOG_DIR" ]; then
+	echo "log directory doesn't exist, so make directory."
+	mkdir $SRC_DIR/log
+fi
+cd $SRC_DIR
+ls > $LOG_DIR/log
+COMMENT
+
+for file in */ ;
+do
+	echo -e "We are in $file";
+	cd $SRC_DIR/$file
+
+	if [ -d $SRC_DIR/$file/.git ]; then
+		git branch; git pull
+		echo -e "$file update completed\n"
+	else
+		echo -e "Not git directory. Pass~\n"
+		continue;
+	fi
+done
+
